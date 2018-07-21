9e94ae704773e581be606eb0ffaed77b6d0d8139
Sven Ruppert <sven.ruppert@gmail.com>
2018-07-21 07:00:49 +0200

    jdk 10.0.2

--- images/maven-3.5-jdk-openjdk-10/build.sh
+++ images/maven-3.5-jdk-openjdk-10/build.sh
@@ -2,6 +2,6 @@
 docker build -t svenruppert/maven-3.5-jdk-openjdk-10 .
 docker push svenruppert/maven-3.5-jdk-openjdk-10:latest
 
-docker tag svenruppert/maven-3.5-jdk-openjdk-10:latest svenruppert/maven-3.5-jdk-openjdk-10:1.10.1
-docker push svenruppert/maven-3.5-jdk-openjdk-10:1.10.1
+docker tag svenruppert/maven-3.5-jdk-openjdk-10:latest svenruppert/maven-3.5-jdk-openjdk-10:1.10.2
+docker push svenruppert/maven-3.5-jdk-openjdk-10:1.10.2
 
