50a0df4c21921821014862ec6c00b9cf58fca093
Sven Ruppert <sven.ruppert@gmail.com>
2018-07-21 06:48:57 +0200

    maven 3.5.4

--- images/maven-3.5-no-jdk/build.sh
+++ images/maven-3.5-no-jdk/build.sh
@@ -2,6 +2,6 @@
 docker build -t svenruppert/maven-3.5-no-jdk .
 
 docker push svenruppert/maven-3.5-no-jdk:latest
-docker tag svenruppert/maven-3.5-no-jdk:latest svenruppert/maven-3.5-no-jdk:maven-3.5.3
-docker push svenruppert/maven-3.5-no-jdk:maven-3.5.3
+docker tag svenruppert/maven-3.5-no-jdk:latest svenruppert/maven-3.5-no-jdk:maven-3.5.4
+docker push svenruppert/maven-3.5-no-jdk:maven-3.5.4
 
