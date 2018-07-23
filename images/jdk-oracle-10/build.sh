f3c4992e7a063822c0bc74792d93a07fe55f74da
Sven Ruppert <sven.ruppert@gmail.com>
2018-07-23 09:11:55 +0200

    added jdk oracle

---
+++ images/jdk-oracle-10/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker build -t svenruppert/jdk-oracle-10 .
+#docker push svenruppert/jdk-oracle-10:latest
+
+docker tag svenruppert/jdk-oracle-10:latest svenruppert/jdk-oracle-10:1.10.0-2
+#docker push svenruppert/jdk-oracle-10:1.10.0-2
+
---
+++ images/jdk-oracle-11/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker build -t svenruppert/jdk-oracle-11 .
+#docker push svenruppert/jdk-oracle-11:latest
+
+docker tag svenruppert/jdk-oracle-11:latest svenruppert/jdk-oracle-11:1.11.0-23
+#docker push svenruppert/jdk-oracle-11:1.11.0-23
+
