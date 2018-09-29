bd86a48904bc0001fdc289715512aa445ae14f63
Sven Ruppert <sven.ruppert@gmail.com>
2018-09-29 19:38:08 +0200

    version updates, added docker based scripts for deploy and compile

--- images/jdk-zulu-10/build.sh
+++ images/jdk-zulu-10/build.sh
@@ -2,6 +2,6 @@
 docker build -t svenruppert/jdk-zulu-10 .
 docker push svenruppert/jdk-zulu-10:latest
 
-docker tag svenruppert/jdk-zulu-10:latest svenruppert/jdk-zulu-10:1.10.0-1
-docker push svenruppert/jdk-zulu-10:1.10.0-1
+docker tag svenruppert/jdk-zulu-10:latest svenruppert/jdk-zulu-10:1.10.0-2
+docker push svenruppert/jdk-zulu-10:1.10.0-2
 
